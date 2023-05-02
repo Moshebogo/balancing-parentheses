6d6a1ff306328305ec58b4e10e5ba19678dcbfef
Eli Moshe <moshebogo@gmail.com>
2023-05-02 00:57:51 -0400

    some tests passing

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,6 +1,35 @@
+const leftParentheses = []
+const rightParentheses = []
+
 function balancingParentheses(string) {
-  // type your code here
+
+      for (parentheses of string) {
+        if (parentheses === '(') {
+            leftParentheses.push(parentheses)
+        } else if (parentheses === ')') {
+            rightParentheses.push(parentheses)
+        }
+  }  
+
+  if (Math.abs(leftParentheses.length - rightParentheses.length >= 3)) {
+      return Math.abs(leftParentheses.length - rightParentheses.length)
+  }
+
+  let total = 0
+  if (string[0] === ')' && string.endsWith('(')) {
+       total = total + 2
+  } else if (string[0] === ')') {
+       total = total + 1
+  } else if (string.endsWith('(')) {
+      total = total + 1
+  }
+  return total
 }
+
+console.log(balancingParentheses(')))))'))
+// console.log(rightParentheses)
+// console.log(leftParentheses)
+
 
 if (require.main === module) {
   // add your own tests in here
